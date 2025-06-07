7e04b73cca47f225bc921b84ab6447fb431f9dbc
Georg Plaz <georg.plaz@gmail.com>
2025-06-07 11:32:53 +0200

    Fix flake8 errors

--- src/pybind/emitters.py
+++ src/pybind/emitters.py
@@ -1,21 +1,26 @@
 from abc import ABC, abstractmethod
 from typing import TypeVar, Generic
 
+
 T = TypeVar("T")
 U = TypeVar("U")
 S = TypeVar("S")
+
 
 class Emitter(ABC):
     @abstractmethod
     def __call__(self) -> None: ...
 
+
 class ValueEmitter(Generic[T], ABC):
     @abstractmethod
     def __call__(self, value0: T) -> None: ...
 
+
 class BiEmitter(Generic[T, U], ABC):
     @abstractmethod
     def __call__(self, value0: T, value1: U) -> None: ...
+
 
 class TriEmitter(Generic[T, U, S], ABC):
     @abstractmethod
