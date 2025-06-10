b5d9f6e4fe60b2e4dac043f3640f431311eba69d
FancyNeuron <58030657+FancyNeuron@users.noreply.github.com>
2025-06-10 17:54:49 +0200

    38 move value tests to separate folders for each type (#49)
    * Restructure test folder architecture
    
    * Rename test files

---
+++ tests/test_values/test_int_values/test_add_int_values.py
@@ -0,0 +1,58 @@
+from spellbind.float_values import FloatVariable
+from spellbind.int_values import IntVariable
+
+
+def test_add_int_values_values():
+    v0 = IntVariable(1)
+    v1 = IntVariable(2)
+    v2 = v0 + v1
+    assert v2.value == 3
+
+    v0.value = 3
+    assert v2.value == 5
+
+
+def test_add_int_value_plus_int_values():
+    v0 = IntVariable(1)
+    v2 = v0 + 2
+    assert v2.value == 3
+
+    v0.value = 3
+    assert v2.value == 5
+
+
+def test_add_int_value_plus_float():
+    v0 = IntVariable(3)
+    v2 = v0 + 2.5
+    assert v2.value == 5.5
+
+    v0.value = 4
+    assert v2.value == 6.5
+
+
+def test_add_int_value_plus_float_value():
+    v0 = IntVariable(3)
+    v1 = FloatVariable(2.5)
+    v2 = v0 + v1
+    assert v2.value == 5.5
+
+    v0.value = 4
+    assert v2.value == 6.5
+
+
+def test_add_int_plus_int_value():
+    v1 = IntVariable(2)
+    v2 = 3 + v1
+    assert v2.value == 5
+
+    v1.value = 4
+    assert v2.value == 7
+
+
+def test_add_float_plus_int_value():
+    v1 = IntVariable(2)
+    v2 = 3.5 + v1
+    assert v2.value == 5.5
+
+    v1.value = 4
+    assert v2.value == 7.5
