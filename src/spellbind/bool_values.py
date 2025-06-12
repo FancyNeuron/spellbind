f774f25b392abd2cf6f3e7f8a27860242cb3af6f
Georg Plaz <georg.plaz@gmail.com>
2025-06-12 15:06:03 +0200

    Add logical operators to BoolValue

--- src/spellbind/bool_values.py
+++ src/spellbind/bool_values.py
@@ -4,14 +4,34 @@ import operator
 from abc import ABC
 from typing import TypeVar, Generic
 
-from spellbind.values import Value, OneToOneValue, Constant, SimpleVariable
+from spellbind.values import Value, OneToOneValue, Constant, SimpleVariable, TwoToOneValue
 
 _S = TypeVar('_S')
+
+BoolLike = Value[bool] | bool
 
 
 class BoolValue(Value[bool], ABC):
     def logical_not(self) -> BoolValue:
         return NotBoolValue(self)
+
+    def __and__(self, other: BoolLike) -> BoolValue:
+        return AndBoolValues(self, other)
+
+    def __rand__(self, other: bool) -> BoolValue:
+        return AndBoolValues(other, self)
+
+    def __or__(self, other: BoolLike) -> BoolValue:
+        return OrBoolValues(self, other)
+
+    def __ror__(self, other: bool) -> BoolValue:
+        return OrBoolValues(other, self)
+
+    def __xor__(self, other: BoolLike) -> BoolValue:
+        return XorBoolValues(self, other)
+
+    def __rxor__(self, other: bool) -> BoolValue:
+        return XorBoolValues(other, self)
 
 
 class OneToBoolValue(OneToOneValue[_S, bool], BoolValue, Generic[_S]):
@@ -21,6 +41,21 @@ class OneToBoolValue(OneToOneValue[_S, bool], BoolValue, Generic[_S]):
 class NotBoolValue(OneToOneValue[bool, bool], BoolValue):
     def __init__(self, value: Value[bool]):
         super().__init__(operator.not_, value)
+
+
+class AndBoolValues(TwoToOneValue[bool, bool, bool], BoolValue):
+    def __init__(self, left: BoolLike, right: BoolLike):
+        super().__init__(operator.and_, left, right)
+
+
+class OrBoolValues(TwoToOneValue[bool, bool, bool], BoolValue):
+    def __init__(self, left: BoolLike, right: BoolLike):
+        super().__init__(operator.or_, left, right)
+
+
+class XorBoolValues(TwoToOneValue[bool, bool, bool], BoolValue):
+    def __init__(self, left: BoolLike, right: BoolLike):
+        super().__init__(operator.xor, left, right)
 
 
 class BoolConstant(BoolValue, Constant[bool]):
